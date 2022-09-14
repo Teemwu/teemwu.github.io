631f6b604b15eb2bbd8619bd9e3c49941440ec1a
teemwu <distinctwu@gmail.com>
2022-09-14 20:51:51 +0800

    feat: add tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+      autoprefixer: { "flexbox": "no-2009" },
+      features: { "custom-properties": false },
+      stage: 3
+    },
+  },
+}
