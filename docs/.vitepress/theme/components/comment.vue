ce2855876de615554b0c2db27a0497e27085aeb1
teemwu <distinctwu@gmail.com>
2022-09-29 11:20:16 +0800

    udpate: md

--- docs/.vitepress/theme/components/comment.vue
+++ docs/.vitepress/theme/components/comment.vue
@@ -1,10 +1,8 @@
 <template>
-	<div class="giscus"></div>
+  <div class="giscus mt-6"></div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 
