7c2334e5cbbdc868d59d6ff82e4e1071a1abf8ab
teemwu <distinctwu@gmail.com>
2022-12-05 10:29:48 +0800

    build: 1. add comment 2. update deploy.yml

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,19 +1,19 @@
 <template>
 	<Layout>
-		<!-- <template #doc-after>
+		<template #doc-after>
 			<Comment v-if="!hideCommentRef" />
-		</template> -->
+		</template>
 	</Layout>
 </template>
 
 <script lang="ts" setup>
 import DefaultTheme from 'vitepress/theme'
-// import Comment from './components/comment.vue'
-// import { useGiscus } from './hooks/useGiscus'
+import Comment from './components/comment.vue'
+import { useGiscus } from './hooks/useGiscus'
 import { useGoogleAnalytics } from './hooks/useGooogleAnalytics'
 
 useGoogleAnalytics()
 
 const { Layout } = DefaultTheme
-// const { hideCommentRef } = useGiscus()
+const { hideCommentRef } = useGiscus()
 </script>
